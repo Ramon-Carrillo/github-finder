b872e3fca8f241012e1022a57100394c5f2647f8
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-09-30 23:30:14 -0500

    added navbar and user container with 3 hard coded users.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/user/Users';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hello World!</h1>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
