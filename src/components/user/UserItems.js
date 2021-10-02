fe8416a36e9acaaaea73930b19b05c5367d19f5e
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-10-01 21:30:09 -0500

    refactoring of code and added env.local for api search

--- src/components/user/UserItems.js
+++ src/components/user/UserItems.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItems extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img src={avatar_url} alt='Avatar' className='round-img' style={{ width: '60px' }} />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItems = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img src={avatar_url} alt='Avatar' className='round-img' style={{ width: '60px' }} />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItems.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItems;
